04c074e8bf5c16a429b896c5502bb62b237530d9
tkoen93 <timokoen93@gmail.com>
2020-04-06 08:34:43 +0200

    Create string to dots pipe

---
+++ src/app/convert-string-to-dots.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ConvertStringToDotsPipe } from './convert-string-to-dots.pipe';
+
+describe('ConvertStringToDotsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ConvertStringToDotsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
