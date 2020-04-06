02f9c358457b4f1daf63b4cf090e97d4accbafbe
tkoen93 <timokoen93@gmail.com>
2020-04-06 08:34:05 +0200

    Add Vacancy class

---
+++ src/app/vacancy.ts
@@ -0,0 +1,12 @@
+export class Vacancy {
+    id: string;
+    vacancyURL: string;
+    title: string;
+    broker: string;
+    vacancyNumber: string;
+    hours: number;
+    location: string;
+    postingDate: string;
+    about: string;
+    skillSet: any[];
+}
