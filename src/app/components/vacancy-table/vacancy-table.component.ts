bae15d06ce9c0391c085c71af7ec710a898b484c
tkoen93 <timokoen93@gmail.com>
2020-06-10 14:52:46 +0200

    Hide/show filter section. Add getAllVacancies function

--- src/app/components/vacancy-table/vacancy-table.component.ts
+++ src/app/components/vacancy-table/vacancy-table.component.ts
@@ -1,23 +1,51 @@
-import { Component, OnInit, Output, Input } from '@angular/core';
+import { Component, OnInit, Output, Input, OnChanges } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { FilterService } from 'src/app/services/filter.service';
+import { IVacancies } from 'src/app/models/ivacancies';
 
 @Component({
   selector: 'app-vacancy-table',
   templateUrl: './vacancy-table.component.html',
-  styleUrls: ['./vacancy-table.component.scss']
+  styleUrls: ['./vacancy-table.component.scss'],
+  providers: [FilterService]
 })
-export class VacancyTableComponent implements OnInit {
+export class VacancyTableComponent implements OnInit, OnChanges {
 
   @Input() isShow: boolean;
   @Output() filterButtonClicked = new EventEmitter();
 
-  constructor() { }
+  displayedColumns: string[] = ['title', 'broker', 'location', 'postingDate', 'openVacancyURL'];
+  vacancies: IVacancies[] = [];
+  showClass: string;
 
-  ngOnInit(): void {
+  constructor(private filterService: FilterService) {
   }
 
-  resizeFilterClick() {
+  ngOnChanges(): void {
+    this.showClass = this.isShow ? 'table-container' : 'table-container-with-filter';
+  }
+
+  ngOnInit(): void {
+    this.getAllVacancies();
+  }
+
+  public resizeFilterClick(): void {
     this.filterButtonClicked.emit();
+  }
+
+  private getAllVacancies(): void {
+    this.filterService.showAllVacancies().subscribe((data: any) => {
+      data.vacancies.forEach(vacancy => {
+        this.vacancies.push({
+            title: vacancy.title,
+            broker: vacancy.broker,
+            postingDate: vacancy.postingDate,
+            location: vacancy.location,
+            url: vacancy.id,
+            vacancyUrl: vacancy.vacancyURL
+        });
+      });
+    });
   }
 
 }
