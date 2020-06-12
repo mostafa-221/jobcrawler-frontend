b582e3187a2b04251fcca7848ad7e32a9622538c
tkoen93 <timokoen93@gmail.com>
2020-06-12 16:44:07 +0200

    Remove VacancyDetailsComponent from routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {VacancyDetailsComponent} from './components/vacancy-details/vacancy-details.component';
 import { FilterComponent } from './components/filter/filter.component';
 
 const routes: Routes = [
     {path: '', component: FilterComponent},
-    {path: 'id/:id', component: VacancyDetailsComponent}
 ];
 
 @NgModule({
